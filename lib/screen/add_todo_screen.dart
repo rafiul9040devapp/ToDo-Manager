913f3e667f2aedcb9330d9b5a7ef6a48a6abeadb
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-01-25 16:31:58 +0600

    whole CRUD is implemented

--- lib/screen/add_todo_screen.dart
+++ lib/screen/add_todo_screen.dart
@@ -2,71 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:todo_manager/model/todo.dart';
 
 class AddTodoScreen extends StatelessWidget {
+  final Todo? todo;
 
-  final Function(Todo) addNewTodo;
-
-  AddTodoScreen(this.addNewTodo, {super.key});
+  AddTodoScreen({Key? key, this.todo}) : super(key: key);
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final TextEditingController _titleTEController = TextEditingController();
 
-  final TextEditingController _descriptionTEController = TextEditingController();
+  final TextEditingController _descriptionTEController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    if (todo != null) {
+      _titleTEController.text = todo?.title ?? '';
+      _descriptionTEController.text = todo?.description ?? '';
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Todo'),
+        title: Text(todo == null ? 'Add Todo' : 'Edit Todo'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextFormField(
                 controller: _titleTEController,
-                decoration: const InputDecoration(
-                  hintText: 'Title'
-                ),
-                validator: (String? value){
-                  final v = value ?? '';
-                  if(v.trim().isEmpty){
-                    return 'Enter Your Title';
-                  }
-                   return null;
+                decoration: const InputDecoration(hintText: 'Title'),
+                validator: (String? value) {
+                  return validationOfInput(value, 'Enter Your Title');
                 },
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(
+                height: 16,
+              ),
               TextFormField(
                 controller: _descriptionTEController,
                 maxLines: 5,
                 maxLength: 100,
-                decoration: const InputDecoration(
-                    hintText: 'Title'
-                ),
-                validator: (String? value){
-                  final v = value ?? '';
-                  if(v.trim().isEmpty){
-                    return 'Enter Your Description';
-                  }
-                  return null;
+                decoration: const InputDecoration(hintText: 'Title'),
+                validator: (String? value) {
+                  return validationOfInput(value, 'Enter Your Description');
                 },
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(
+                height: 16,
+              ),
               SizedBox(
                 width: MediaQuery.sizeOf(context).width,
                 child: ElevatedButton(
-                  onPressed: (){
-                    if(_formKey.currentState!.validate()){
-                      addNewTodo( Todo(_titleTEController.text,_descriptionTEController.text,DateTime.now()));
-                      Navigator.pop(context);
-                    }
+                  onPressed: () {
+                    addingOrUpdatingTheList(context);
                   },
-                  child: Text('Add'),
+                  child: Text(todo == null ? 'Save' : 'Update'),
                 ),
               )
             ],
@@ -74,5 +66,26 @@ class AddTodoScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  String? validationOfInput(String? value, String errorMessage) {
+    final v = value ?? '';
+    if (v.trim().isEmpty) {
+      return errorMessage;
+    }
+    return null;
+  }
+
+  void addingOrUpdatingTheList(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      Navigator.pop(
+        context,
+        Todo(
+          _titleTEController.text,
+          _descriptionTEController.text,
+          DateTime.now(),
+        ),
+      );
+    }
   }
 }
--- lib/screen/todo_list_screen.dart
+++ lib/screen/todo_list_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:todo_manager/model/todo.dart';
+import 'package:todo_manager/model/todo_service.dart';
 import 'package:todo_manager/screen/add_todo_screen.dart';
 
 class TodoListScreen extends StatefulWidget {
   const TodoListScreen({super.key});
+
   @override
   State<TodoListScreen> createState() => _TodoListScreenState();
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
-  List<Todo> todoList = [];
+  final ToDoService _toDoService = ToDoService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,18 +20,18 @@ class _TodoListScreenState extends State<TodoListScreen> {
         title: const Text('Todo'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => AddTodoScreen(addItem)));
-        },
+        onPressed: () async => await addToList(context),
         backgroundColor: Colors.amberAccent,
         foregroundColor: Colors.white,
-        child: Icon(Icons.add,size: 25,),
+        child: const Icon(
+          Icons.add,
+          size: 25,
+        ),
       ),
-
       body: ListView.separated(
-        itemCount: todoList.length,
+        itemCount: _toDoService.listOfTodo.length,
         itemBuilder: (context, index) {
-          final item = todoList[index];
+          final item = _toDoService.listOfTodo[index];
           return ListTile(
             title: Text(item.title),
             subtitle: Column(
@@ -42,7 +45,7 @@ class _TodoListScreenState extends State<TodoListScreen> {
               children: [
                 IconButton(
                   onPressed: () {
-                    removeItem(index);
+                    removeFromList(index);
                   },
                   icon: const Icon(
                     Icons.delete_forever_outlined,
@@ -51,8 +54,8 @@ class _TodoListScreenState extends State<TodoListScreen> {
                   ),
                 ),
                 IconButton(
-                  onPressed: () {
-
+                  onPressed: () async {
+                    await updateTheList(context, item, index);
                   },
                   icon: const Icon(
                     Icons.edit,
@@ -75,15 +78,85 @@ class _TodoListScreenState extends State<TodoListScreen> {
     );
   }
 
-
-  void addItem(Todo todo){
-    todoList.insert(0,todo);
-    setState(() {});
+  Future<void> updateTheList(BuildContext context, Todo item, int index) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddTodoScreen(
+          todo: item,
+        ),
+      ),
+    );
+    if (result != null && result is Todo) {
+      _toDoService.updateItemOfList(result, index);
+      setState(() {});
+    }
   }
 
-  void removeItem(int index){
-    todoList.removeAt(index);
-    setState(() {});
+  Future<void> addToList(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddTodoScreen(),
+      ),
+    );
+    if (result != null && result is Todo) {
+      _toDoService.addItemToList(result);
+      setState(() {});
+    }
   }
 
+  void removeFromList(int index) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text(
+          'Delete The Todo!!!!',
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        content: const Text('Are you Sure About this?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              _toDoService.removeItemOfList(index);
+              setState(() {});
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'Ok',
+              style: TextStyle(
+                color: Colors.greenAccent,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'Cancel',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+    );
+  }
 }
